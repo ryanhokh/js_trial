646abc02fbcc0c664f2045e2dfe99c336a7e0b14
Ryan <hokai_999@hotmail.com>
2021-10-23 01:56:40 +0800

    updated index.html and test.js

---
+++ test.js
@@ -0,0 +1,23 @@
+let x = 10
+
+console.log(x, typeof(x))
+
+
+let y = true
+console.log(y, typeof((y)))
+
+let b
+console.log(b)
+
+let ls = [1,2,3,4, 'xxx']
+console.log(ls)
+
+let person = {
+    v: 'hihih',
+    val: 20,
+    dead: true
+}
+
+console.log(person['v'])
+console.log(ls[4][0])
+console.log(`${ls} yoyoyo ${x}`)
